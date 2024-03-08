474e794b70960dd13f3b7d31817be1741b234f7b
Kasra Farmer <kasra.keshavarz@usask.ca>
2024-03-08 18:29:02 -0500

    Making a private function for greating a DiGraph out of river networks

--- src/hydrant/topology/river_graph.py
+++ src/hydrant/topology/river_graph.py
@@ -13,6 +13,19 @@ from typing import (
 )
 
 
+def _return_nx_graph(
+    gdf: gpd.GeoDataFrame,
+    main_id: str,
+    ds_main_id: str,
+) -> nx.DiGraph:
+    '''Return networkx DiGraph of the input river network
+    '''
+    return nx.from_pandas_edgelist(gdf,
+                                   source=main_id,
+                                   target=ds_main_id,
+                                   create_using=nx.DiGraph)
+
+
 def find_upstream(
     gdf: gpd.GeoDataFrame,
     target_id: Union[str, int, ...],
@@ -21,7 +34,7 @@ def find_upstream(
 ) -> set[...]:
     '''Find "ancestors" or upstream segments in a river network given
     in the from of a geopandas.GeoDataFrame `gdf`
-    
+
     Parameters
     ----------
     gdf: geopandas.GeoDataFrame
@@ -37,24 +50,66 @@ def find_upstream(
     ds_main_id: str
         String defining the column of downstream element IDs in the
         input geopandas dataframe
-    
+
     Returns
     -------
     nodes: list
         IDs of nodes being upstream or anscestor of the `target_id`
-    
+
     '''
     # creating a DiGraph out of `gdf` object
-    riv_graph = nx.from_pandas_edgelist(gdf,
-                                        source=main_id,
-                                        target=ds_main_id,
-                                        create_using=nx.DiGraph)
+    riv_graph = _return_nx_graph(gdf,
+                                 main_id,
+                                 ds_main_id)
 
     # return nodes in a list
     nodes = nx.ancestors(riv_graph, target_id)
 
     # adding `target_id` as the last node of the branch
     nodes.add(target_id)
+
+    return nodes
+
+
+def find_downstream(
+    gdf: gpd.GeoDataFrame,
+    target_id: Union[str, int, ...],
+    main_id: str,
+    ds_main_id: str,
+) -> set[...]:
+    '''
+    Returns "descendants" or downstream segments in a river network given
+    in the from of a geopandas.GeoDataFrame (`gdf`)
+
+    Parameters
+    ----------
+    gdf: geopandas.GeoDataFrame
+        GeoDataFrame of river segments including at least three pieces
+        of information: 1) geometries of segments, 2) segment IDs, and
+        3) downstream segment IDs
+    target_id: str, int, or any other data type as included in `gdf`
+        Indicating the target ID anscestor or upstream of which is
+        desired
+    main_id: str
+        String defining the column of element IDs in the input geopandas
+        dataframe
+    ds_main_id: str
+        String defining the column of downstream element IDs in the
+        input geopandas dataframe
+
+    Returns
+    -------
+    nodes: list
+        IDs of nodes being upstream or anscestor of the `target_id`
+    '''
+
+    # creating a DiGraph out of `gdf` object
+    riv_graph = _return_nx_graph(gdf,
+                                 main_id,
+                                 ds_main_id)
+
+    # return nodes in a list
+    nodes = nx.descendants(riv_graph, target_id)
 
     return nodes
 
@@ -77,17 +132,14 @@ def longest_branch(
     ds_main_id : str or int, defaults to `None`
         column label within `riv` corresponding to downstream segments of
         each river
-        
+
     Returns
     -------
     nodes : set
         a set object containing river segment `main_id` calues of the
         longest branch found in `riv`
     """
-    riv_graph = nx.from_pandas_edgelist(riv,
-                                        source=main_id,
-                                        target=ds_main_id,
-                                        create_using=nx.DiGraph)
+    riv_graph = nx.from_pandas_edgelist(riv, main_id, ds_main_id)
 
     nodes = nx.dag_longest_path(riv_graph)
 
